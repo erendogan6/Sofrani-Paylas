5cc11a5311ad810701ab7b1eaf719b54b5968397
erendogan6 <erendogan6@hotmail.com>
2024-05-02 14:23:12 +0300

    feat(login): added AndroidEntryPoint to LoginActivity

--- app/src/main/java/com/erendogan6/sofranipaylas/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/ui/activity/LoginActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.erendogan6.sofranipaylas.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
