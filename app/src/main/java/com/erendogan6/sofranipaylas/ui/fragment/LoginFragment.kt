4268eb6e918ad72135af0d88b37fdebc9d1108e1
erendogan6 <erendogan6@hotmail.com>
2024-05-02 14:24:08 +0300

    feat(login): implemented LoginFragment with view binding and view model

---
+++ app/src/main/java/com/erendogan6/sofranipaylas/ui/fragment/LoginFragment.kt
@@ -0,0 +1,47 @@
+package com.erendogan6.sofranipaylas.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.erendogan6.sofranipaylas.R
+import com.erendogan6.sofranipaylas.databinding.FragmentLoginBinding
+import com.erendogan6.sofranipaylas.viewmodel.UserViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginFragment : Fragment() {
+    private val viewModel: UserViewModel by viewModels()
+    private lateinit var binding: FragmentLoginBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString().trim()
+            val password = binding.passwordEditText.text.toString().trim()
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(requireActivity(), "Email and password cannot be empty.", Toast.LENGTH_SHORT).show()
+            } else {
+                viewModel.login(email, password)
+            }
+        }
+
+        viewModel.userLoginResult.observe(viewLifecycleOwner) { user ->
+            if (user != null) {
+                findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+            } else {
+                Toast.makeText(requireActivity(), "Login failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.registerTextView.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        return binding.root
+    }
+}
