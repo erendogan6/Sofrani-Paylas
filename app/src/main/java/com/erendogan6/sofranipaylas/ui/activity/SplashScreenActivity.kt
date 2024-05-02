fbefae47f15bc0d4cdb847060c3fb34f1fd16f5f
erendogan6 <erendogan6@hotmail.com>
2024-05-02 14:33:06 +0300

    feat(splash-screen): added SplashScreenActivity with initial setup

--- app/src/main/java/com/erendogan6/sofranipaylas/ui/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/ui/activity/SplashScreenActivity.kt
@@ -1,16 +1,17 @@
-package com.erendogan6.sofranipaylas
+package com.erendogan6.sofranipaylas.ui.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.erendogan6.sofranipaylas.R
 
-class MainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
