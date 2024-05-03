9282bf6ebbdaa00e655f708d0a163af4bee09869
erendogan6 <erendogan6@hotmail.com>
2024-05-03 09:25:43 +0300

    feat(di): added FirebaseStorage provider to AppModule

--- app/src/main/java/com/erendogan6/sofranipaylas/di/AppModule.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/di/AppModule.kt
@@ -1,8 +1,9 @@
 package com.erendogan6.sofranipaylas.di
 
-import com.erendogan6.sofranipaylas.repository.UserRepository
+import com.erendogan6.sofranipaylas.repository.Repository
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,7 +16,7 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun provideUserRepository(firebaseAuth: FirebaseAuth, firestore: FirebaseFirestore): UserRepository {
-        return UserRepository(firebaseAuth, firestore)
+    fun provideUserRepository(firebaseAuth: FirebaseAuth, firestore: FirebaseFirestore, storage: FirebaseStorage): Repository {
+        return Repository(firebaseAuth, firestore, storage)
     }
 }
--- app/src/main/java/com/erendogan6/sofranipaylas/di/FirebaseModule.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/di/FirebaseModule.kt
@@ -2,6 +2,7 @@ package com.erendogan6.sofranipaylas.di
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,4 +20,8 @@ object FirebaseModule {
     @Singleton
     @Provides
     fun provideFirebaseFirestore(): FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    @Singleton
+    @Provides
+    fun provideFirebaseStorage(): FirebaseStorage = FirebaseStorage.getInstance()
 }
