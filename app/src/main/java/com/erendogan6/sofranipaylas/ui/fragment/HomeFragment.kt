bfec074e9903fbce311d3718601d628788ed9e45
erendogan6 <erendogan6@hotmail.com>
2024-05-03 13:13:48 +0300

    feat(fragment): integrated HomeViewModel to fetch posts and display them using HomeAdapter in HomeFragment

--- app/src/main/java/com/erendogan6/sofranipaylas/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/ui/fragment/HomeFragment.kt
@@ -5,23 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.erendogan6.sofranipaylas.adapter.HomeAdapter
 import com.erendogan6.sofranipaylas.databinding.FragmentHomeBinding
-import com.erendogan6.sofranipaylas.model.Event
+import com.erendogan6.sofranipaylas.viewmodel.HomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
-
+    private val viewModel: HomeViewModel by viewModels()
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private val adapter = HomeAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val list: ArrayList<Event> = arrayListOf()
-        list.add(Event(null, "test", "1", "true", "2", "yok", null, 5, listOf(), "Selam"))
-        binding.homeRecyclerView.adapter = HomeAdapter(list)
-        binding.homeRecyclerView.layoutManager = LinearLayoutManager(requireActivity())
+        setupRecyclerView()
+        observePosts()
         return binding.root
+    }
+
+    private fun setupRecyclerView() {
+        binding.homeRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = this@HomeFragment.adapter
+        }
+    }
+    
+    private fun observePosts() {
+        lifecycleScope.launch {
+            viewModel.posts.observe(viewLifecycleOwner) { posts ->
+                adapter.submitList(posts)
+            }
+        }
     }
 
     override fun onDestroyView() {
