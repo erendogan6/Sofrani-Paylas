b56d0d1ac3268955a39792ee5b2f8a2ccc126d7b
erendogan6 <erendogan6@hotmail.com>
2024-05-03 13:12:59 +0300

    refactor(adapter): upgraded HomeAdapter to use ListAdapter for improved performance and enhancements

--- app/src/main/java/com/erendogan6/sofranipaylas/adapter/HomeAdapter.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/adapter/HomeAdapter.kt
@@ -2,26 +2,37 @@ package com.erendogan6.sofranipaylas.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.erendogan6.sofranipaylas.databinding.EventItemBinding
-import com.erendogan6.sofranipaylas.model.Event
+import com.erendogan6.sofranipaylas.model.Post
 
-class HomeAdapter(val list: ArrayList<Event>) : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
+class HomeAdapter : ListAdapter<Post, HomeAdapter.ViewHolder>(DiffCallback()) {
 
-
-    inner class ViewHolder(val binding: EventItemBinding) : RecyclerView.ViewHolder(binding.root)
+    class ViewHolder(private val binding: EventItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(post: Post) {
+            binding.eventTitle.text = post.title
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = EventItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-        return list.size
-    }
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.eventTitle.text = list.get(position).title
+        val post = getItem(position)
+        holder.bind(post)
     }
 
+    class DiffCallback : DiffUtil.ItemCallback<Post>() {
+        override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean {
+            return oldItem.title == newItem.title
+        }
+
+        override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean {
+            return oldItem == newItem
+        }
+    }
 }
