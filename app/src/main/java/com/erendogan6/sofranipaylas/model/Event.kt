7ee255f832b20fcb9a5c70076a8c42b946ac168b
erendogan6 <erendogan6@hotmail.com>
2024-05-03 09:26:19 +0300

    refactor(models): updated Post and Event models to align with current requirements

--- app/src/main/java/com/erendogan6/sofranipaylas/model/Event.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/model/Event.kt
@@ -1,16 +1,6 @@
 package com.erendogan6.sofranipaylas.model
 
 import com.google.firebase.Timestamp
-data class Event(
-    val date: Timestamp,
-    val description: String,
-    val eventID: String,
-    val eventStatus: String,
-    val hostID: String,
-    val images: String,
-    val location: Timestamp,
-    val maxParticipants: Int,
-    val participantIDs: List<String>,
-    val time: String,
-    val title: String
-)
+import com.google.firebase.firestore.GeoPoint
+
+data class Event(val date: Timestamp?, val description: String, val eventID: String, val eventStatus: String, val hostID: String, val images: String, val location: GeoPoint?, val maxParticipants: Int, val participantIDs: List<String>, val title: String)
