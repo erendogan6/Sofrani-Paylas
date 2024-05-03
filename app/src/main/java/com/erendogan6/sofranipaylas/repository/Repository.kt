9c1190ee8651b30677d6af226b6ce4c2963ee5c7
erendogan6 <erendogan6@hotmail.com>
2024-05-03 09:24:54 +0300

    feat(repository): added functions for image uploading and post sharing to Repository

--- app/src/main/java/com/erendogan6/sofranipaylas/repository/Repository.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/repository/Repository.kt
@@ -1,6 +1,9 @@
 package com.erendogan6.sofranipaylas.repository
 
+import android.net.Uri
+import com.erendogan6.sofranipaylas.model.Post
 import com.erendogan6.sofranipaylas.model.User
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
@@ -52,4 +55,28 @@ class Repository @Inject constructor(private val firebaseAuth: FirebaseAuth, pri
             emit(false)
         }
     }
+
+
+    suspend fun uploadImageAndGetUrl(imageUri: Uri): Flow<Result<String>> = flow {
+        try {
+            val imageRef = storage.reference.child("images/${imageUri.lastPathSegment}")
+            val uploadTaskSnapshot = imageRef.putFile(imageUri).await()
+            val imageUrl = uploadTaskSnapshot.metadata?.reference?.downloadUrl?.await().toString()
+            emit(Result.success(imageUrl))
+        } catch (e: Exception) {
+            emit(Result.failure(e))
+        }
+    }
+
+    suspend fun submitPost(title: String, description: String, participants: Int, imageUrl: String, date: Timestamp): Flow<Result<Boolean>> = flow {
+        try {
+            val currentUserID = firebaseAuth.currentUser?.uid.orEmpty()
+            val post = Post(title = title, description = description, participants = participants, images = imageUrl, date = date, status = true, userID = currentUserID)
+            firestore.collection("Posts").add(post).await()
+            emit(Result.success(true))
+        } catch (e: Exception) {
+            emit(Result.failure(e))
+        }
+    }
+
 }
