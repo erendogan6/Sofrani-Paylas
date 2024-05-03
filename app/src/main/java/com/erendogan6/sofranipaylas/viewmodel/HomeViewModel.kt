1615dffce1b8108d3932dac3d8b31d1a2b6fcea1
erendogan6 <erendogan6@hotmail.com>
2024-05-03 12:50:48 +0300

    feat(viewmodel): integrated new repository method to fetch posts and updated ViewModel accordingly

--- app/src/main/java/com/erendogan6/sofranipaylas/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/viewmodel/HomeViewModel.kt
@@ -3,11 +3,23 @@ package com.erendogan6.sofranipaylas.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.erendogan6.sofranipaylas.model.Post
+import com.erendogan6.sofranipaylas.repository.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class HomeViewModel : ViewModel() {
+@HiltViewModel
+class HomeViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
+    private val _posts = MutableLiveData<List<Post?>>()
+    val posts: LiveData<List<Post?>> = _posts
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    fun getPosts() {
+        viewModelScope.launch {
+            repository.getPosts().collect() { postList ->
+                _posts.value = postList
+            }
+        }
     }
-    val text: LiveData<String> = _text
 }
