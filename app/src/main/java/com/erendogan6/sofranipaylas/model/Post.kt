93bf30a7dab19fec8ec6e01eea02e8dfeb7d8f38
erendogan6 <erendogan6@hotmail.com>
2024-05-03 12:50:30 +0300

    refactor(model): modified Post model to align with Firebase data fetching requirements

--- app/src/main/java/com/erendogan6/sofranipaylas/model/Post.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/model/Post.kt
@@ -1,5 +1,20 @@
 package com.erendogan6.sofranipaylas.model
 
 import com.google.firebase.Timestamp
+import com.google.firebase.firestore.PropertyName
 
-data class Post(val date: Timestamp, val description: String, val images: String, val participants: Int, val status: Boolean, val title: String, val userID: String)
+data class Post(
+    @PropertyName("date")
+    val date: Timestamp = Timestamp.now(),
+    @PropertyName("description")
+    val description: String = "",
+    @PropertyName("images")
+    val images: String = "",
+    @PropertyName("participants")
+    val participants: Int = 0,
+    @PropertyName("status")
+    val status: Boolean = false,
+    @PropertyName("title")
+    val title: String = "",
+    @PropertyName("userID")
+    val userID: String = "")
