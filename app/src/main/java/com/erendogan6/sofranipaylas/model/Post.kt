5d37caf5c996f866649edbc1d8b019275a8ef303
erendogan6 <erendogan6@hotmail.com>
2024-05-19 18:26:04 +0300

    feat(model): updated Post class with new fields and types for enhanced functionality

--- app/src/main/java/com/erendogan6/sofranipaylas/model/Post.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/model/Post.kt
@@ -1,20 +1,36 @@
 package com.erendogan6.sofranipaylas.model
 
 import com.google.firebase.Timestamp
+import com.google.firebase.firestore.GeoPoint
 import com.google.firebase.firestore.PropertyName
 
 data class Post(
     @PropertyName("date")
     val date: Timestamp = Timestamp.now(),
+
     @PropertyName("description")
     val description: String = "",
+
+    @PropertyName("eventStatus")
+    val eventStatus: Boolean = true,
+
+    @PropertyName("hostID")
+    val hostID: String = "",
+
     @PropertyName("images")
-    val images: String = "",
+    val images: List<String> = listOf(),
+
+    @PropertyName("location")
+    val location: GeoPoint = GeoPoint(0.0, 0.0),
+
+    @PropertyName("maxParticipants")
+    val maxParticipants: Int = 0,
+
     @PropertyName("participants")
-    val participants: Int = 0,
-    @PropertyName("status")
-    val status: Boolean = false,
+    val participants: List<String>? = listOf(),
+
+    @PropertyName("relatedFoods")
+    val relatedFoods: List<String> = listOf(),
+
     @PropertyName("title")
-    val title: String = "",
-    @PropertyName("userID")
-    val userID: String = "")
+    val title: String = "")
