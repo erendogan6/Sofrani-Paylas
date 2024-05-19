df516c16951b8ff2172bbb67c712460298e53087
erendogan6 <erendogan6@hotmail.com>
2024-05-19 18:39:15 +0300

    feat(auth): add logout functionality

--- app/src/main/java/com/erendogan6/sofranipaylas/ui/fragment/ProfileFragment.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/ui/fragment/ProfileFragment.kt
@@ -1,22 +1,30 @@
 package com.erendogan6.sofranipaylas.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.erendogan6.sofranipaylas.databinding.FragmentProfileBinding
-import com.erendogan6.sofranipaylas.viewmodel.ProfileViewModel
+import com.erendogan6.sofranipaylas.ui.activity.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ProfileFragment : Fragment() {
-
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val profileViewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        binding.logoutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
 
         return binding.root
     }
