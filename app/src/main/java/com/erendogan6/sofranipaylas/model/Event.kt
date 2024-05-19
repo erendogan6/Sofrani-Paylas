734afb3ca55f03e6f6972b1d360fe0ac0b82b1c9
erendogan6 <erendogan6@hotmail.com>
2024-05-19 18:31:16 +0300

    chore(model): remove unused Event data model

--- app/src/main/java/com/erendogan6/sofranipaylas/model/Event.kt
+++
@@ -1,6 +0,0 @@
-package com.erendogan6.sofranipaylas.model
-
-import com.google.firebase.Timestamp
-import com.google.firebase.firestore.GeoPoint
-
-data class Event(val date: Timestamp?, val description: String, val eventID: String, val eventStatus: String, val hostID: String, val images: String, val location: GeoPoint?, val maxParticipants: Int, val participantIDs: List<String>, val title: String)
