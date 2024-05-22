c69bb440cae88cd07f6d392de78da3d678fbdc83
erendogan6 <erendogan6@hotmail.com>
2024-05-22 14:01:12 +0300

    feat(viewmodel): add profile image upload functionality to ProfileViewModel

--- app/src/main/java/com/erendogan6/sofranipaylas/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/viewmodel/ProfileViewModel.kt
@@ -1,5 +1,6 @@
 package com.erendogan6.sofranipaylas.viewmodel
 
+import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -14,10 +15,21 @@ class ProfileViewModel @Inject constructor(private val repository: Repository) :
     private val _changePasswordResult = MutableLiveData<Boolean>()
     val changePasswordResult: LiveData<Boolean> get() = _changePasswordResult
 
+    private val _uploadImageResult = MutableLiveData<Result<String>>()
+    val uploadImageResult: LiveData<Result<String>> get() = _uploadImageResult
+
     fun changePassword(currentPassword: String, newPassword: String) {
         viewModelScope.launch {
             repository.changePassword(currentPassword, newPassword).collect { result ->
                 _changePasswordResult.value = result
+            }
+        }
+    }
+
+    fun uploadProfileImage(imageUri: Uri) {
+        viewModelScope.launch {
+            repository.updateProfilePicture(imageUri).collect { result ->
+                _uploadImageResult.value = result
             }
         }
     }
