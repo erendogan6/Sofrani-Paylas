06cf003f1c4a0488f2e7c39066ad73d96e6e97cd
erendogan6 <erendogan6@hotmail.com>
2024-05-22 13:45:07 +0300

    feat(viewmodel): implement change password functionality in ProfileViewModel

--- app/src/main/java/com/erendogan6/sofranipaylas/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/viewmodel/ProfileViewModel.kt
@@ -3,11 +3,22 @@ package com.erendogan6.sofranipaylas.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.erendogan6.sofranipaylas.repository.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ProfileViewModel : ViewModel() {
+@HiltViewModel
+class ProfileViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
+    private val _changePasswordResult = MutableLiveData<Boolean>()
+    val changePasswordResult: LiveData<Boolean> get() = _changePasswordResult
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    fun changePassword(currentPassword: String, newPassword: String) {
+        viewModelScope.launch {
+            repository.changePassword(currentPassword, newPassword).collect { result ->
+                _changePasswordResult.value = result
+            }
+        }
     }
-    val text: LiveData<String> = _text
 }
