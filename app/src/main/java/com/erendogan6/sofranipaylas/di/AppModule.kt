5f4cfcecdaf059531ecc9da0d938a89e66dcbeee
erendogan6 <erendogan6@hotmail.com>
2024-05-23 00:11:30 +0300

    feat(AppModule): add application context to Repository provider for enhanced context-aware operations

--- app/src/main/java/com/erendogan6/sofranipaylas/di/AppModule.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/di/AppModule.kt
@@ -1,5 +1,6 @@
 package com.erendogan6.sofranipaylas.di
 
+import android.content.Context
 import com.erendogan6.sofranipaylas.repository.Repository
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -7,6 +8,7 @@ import com.google.firebase.storage.FirebaseStorage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -16,7 +18,9 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun provideUserRepository(firebaseAuth: FirebaseAuth, firestore: FirebaseFirestore, storage: FirebaseStorage): Repository {
-        return Repository(firebaseAuth, firestore, storage)
+    fun provideUserRepository(firebaseAuth: FirebaseAuth, firestore: FirebaseFirestore, storage: FirebaseStorage,
+                              @ApplicationContext
+                              context: Context): Repository {
+        return Repository(firebaseAuth, firestore, storage, context)
     }
 }
