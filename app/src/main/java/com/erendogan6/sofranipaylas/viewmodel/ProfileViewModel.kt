46a5db56afe6f31ce6b5e1ddc1f1b32519e0ec31
erendogan6 <erendogan6@hotmail.com>
2024-05-22 14:13:20 +0300

    feat(viewmodel): add current user retrieval and result reset functionality to ProfileViewModel

--- app/src/main/java/com/erendogan6/sofranipaylas/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/viewmodel/ProfileViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.erendogan6.sofranipaylas.model.User
 import com.erendogan6.sofranipaylas.repository.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -15,8 +16,11 @@ class ProfileViewModel @Inject constructor(private val repository: Repository) :
     private val _changePasswordResult = MutableLiveData<Boolean>()
     val changePasswordResult: LiveData<Boolean> get() = _changePasswordResult
 
-    private val _uploadImageResult = MutableLiveData<Result<String>>()
-    val uploadImageResult: LiveData<Result<String>> get() = _uploadImageResult
+    private val _uploadImageResult = MutableLiveData<Result<String>?>()
+    val uploadImageResult: MutableLiveData<Result<String>?> get() = _uploadImageResult
+
+    private val _currentUser = MutableLiveData<User?>()
+    val currentUser: LiveData<User?> get() = _currentUser
 
     fun changePassword(currentPassword: String, newPassword: String) {
         viewModelScope.launch {
@@ -32,5 +36,17 @@ class ProfileViewModel @Inject constructor(private val repository: Repository) :
                 _uploadImageResult.value = result
             }
         }
+    }
+
+    fun getCurrentUser() {
+        viewModelScope.launch {
+            repository.getCurrentUser().collect { user ->
+                _currentUser.value = user
+            }
+        }
+    }
+
+    fun resetUploadImageResult() {
+        _uploadImageResult.value = null
     }
 }
