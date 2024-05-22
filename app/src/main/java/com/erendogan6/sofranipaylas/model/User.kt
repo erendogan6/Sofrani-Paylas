7f684594375c1a334e8990bf77c4f4d222daa51e
erendogan6 <erendogan6@hotmail.com>
2024-05-23 01:46:45 +0300

    refactor(User): add PropertyName annotations to all fields

--- app/src/main/java/com/erendogan6/sofranipaylas/model/User.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/model/User.kt
@@ -1,3 +1,23 @@
 package com.erendogan6.sofranipaylas.model
 
-data class User(val about: String = "", val email: String = "", val isHost: Boolean = false, val name: String = "", val phone: String? = null, val profilePicture: String = "", val role: String = "", val surname: String = "", val userName: String = "")
+import com.google.firebase.firestore.PropertyName
+
+data class User(
+    @PropertyName("about")
+    val about: String = "",
+    @PropertyName("email")
+    val email: String = "",
+    @PropertyName("isHost")
+    val isHost: Boolean = false,
+    @PropertyName("name")
+    val name: String = "",
+    @PropertyName("phone")
+    val phone: String? = null,
+    @PropertyName("profilePicture")
+    val profilePicture: String = "",
+    @PropertyName("role")
+    val role: String = "",
+    @PropertyName("surname")
+    val surname: String = "",
+    @PropertyName("userName")
+    val userName: String = "")
