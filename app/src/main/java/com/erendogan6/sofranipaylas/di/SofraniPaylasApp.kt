544688bb6e2f0008435c3622a215499ca4087a1c
erendogan6 <erendogan6@hotmail.com>
2024-05-22 19:59:45 +0300

    refactor(SofraniPaylasApp): add context injection and @ApplicationContext qualifier for Hilt bug fix

--- app/src/main/java/com/erendogan6/sofranipaylas/di/SofraniPaylasApp.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/di/SofraniPaylasApp.kt
@@ -1,7 +1,14 @@
 package com.erendogan6.sofranipaylas.di
 
 import android.app.Application
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
 
 @HiltAndroidApp
-class SofraniPaylasApp: Application()
+class SofraniPaylasApp : Application() {
+    @Inject
+    @ApplicationContext
+    lateinit var context: Context
+}
