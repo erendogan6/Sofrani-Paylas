4beca9acabdfc73ca16a7bbd6972201b15256302
erendogan6 <erendogan6@hotmail.com>
2024-05-22 19:59:00 +0300

    refactor(ShareFragment, Repository, ShareViewModel): move address fetching logic to Repository and update related classes

--- app/src/main/java/com/erendogan6/sofranipaylas/extensions/Extensions.kt
+++ app/src/main/java/com/erendogan6/sofranipaylas/extensions/Extensions.kt
@@ -1,6 +1,9 @@
 package com.erendogan6.sofranipaylas.extensions
 
 import android.content.Intent
+import android.location.Address
+import android.location.Geocoder
+import android.os.Build
 import androidx.fragment.app.Fragment
 import com.erendogan6.sofranipaylas.ui.activity.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
@@ -11,5 +14,19 @@ fun Fragment.checkUserSessionAndNavigate() {
         val intent = Intent(requireContext(), LoginActivity::class.java)
         startActivity(intent)
         requireActivity().finish()
+    }
+}
+
+@Suppress("DEPRECATION")
+fun Geocoder.getAddress(latitude: Double, longitude: Double, address: (Address?) -> Unit) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getFromLocation(latitude, longitude, 1) { address(it.firstOrNull()) }
+        return
+    }
+
+    try {
+        address(getFromLocation(latitude, longitude, 1)?.firstOrNull())
+    } catch (e: Exception) {
+        address(null)
     }
 }
